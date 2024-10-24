2697f8af2deae5c143aec4b205d42219feb594d1
kleysongomes <kleysongomes7@gmail.com>
2024-10-23 23:33:28 -0300

    mudnado versão da unity

---
+++ Game_Version_Unity_1_3f1/Assets/Scripts/Player.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float runSpeed;
+
+    private Rigidbody2D rig;
+
+    private float initialSpeed;
+    private bool _isRunning;
+    private bool _isRolling;
+    private Vector2 _direction;
+
+    public Vector2 direction
+    {
+        get { return _direction; }
+        set { _direction = value; }
+    }
+
+    public bool isRunning
+    {
+        get { return _isRunning; }
+        set { _isRunning = value; }
+    }
+
+    public bool isRolling
+    {
+        get { return _isRolling; }
+        set { _isRolling = value; }
+    }
+
+    private void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        initialSpeed = speed;
+    }
+
+    //Aqui fica os inputs
+    void Update()
+    {
+        OnInput();
+        OnRun();
+        OnRolling();
+    }
+
+    //Aqui fica tudo sobre fisica e afins
+    private void FixedUpdate()
+    {
+        OnMove();
+    }
+
+    #region Movement 
+    void OnInput()
+    {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove()
+    {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    void OnRun()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _isRunning = true;
+            speed = runSpeed;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = initialSpeed;
+            _isRunning = false;
+        }
+    }
+
+    void OnRolling()
+    {
+        if (Input.GetMouseButtonDown(1)) //btn direito
+        {
+            speed = runSpeed;
+            _isRolling = true;
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            speed = initialSpeed;
+            _isRolling = false;
+        }
+    }
+
+    #endregion
+}
